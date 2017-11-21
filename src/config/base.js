8bc17623b75192d8f0004e662416d0f11ed362a1
Koos van Egmond <koos@cream.nl>
2017-11-21 08:05:33 +0100

    Added new sounds

--- src/config/base.js
+++ src/config/base.js
@@ -2,7 +2,10 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-	sounds: {
-		'Thomas!': require('../sounds/thomas.ogg')
-	}
+    sounds: {
+        'Thomas!':    require('../sounds/thomas.ogg'),
+        'Thomas >:(': require('../sounds/thomas_angry.ogg'),
+        'Thomas?':    require('../sounds/thomas_qm.ogg'),
+        'Thomas..':   require('../sounds/thomas_dotdot.ogg')
+    }
 }
